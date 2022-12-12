a0b895715b003ac9bbcdbab1027583f5fd76b63d
TonacaoZach <120362192+TonacaoZach@users.noreply.github.com>
2022-12-12 10:51:34 +0400

    Add files via upload

---
+++ chapter-1.py
@@ -0,0 +1,44 @@
+#Exercise 1
+
+print ("Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are")
+
+print('\n\n')
+
+#Exercise 2
+from platform import python_version
+
+print("Current Python Version-", python_version())
+
+print('\n\n')
+
+#Exercise 3
+
+# importing datetime module for now()
+import datetime
+ 
+# using now() to get current time
+current_time = datetime.datetime.now()
+ 
+# Printing value of now.
+print("Time now at greenwich meridian is:", current_time)
+
+print('\n\n')
+
+#Exercise 4
+
+a = "Hello, my name is"
+b = "Zach. I am a"
+c = "Student of Bathspa."
+
+print(a,b,c)
+
+print('\n\n')
+
+#Exercise 5
+
+rad = int(input('Please enter the radius of the circle: '))
+
+equ = 2*3.141592653589793*rad
+
+print(equ)
+
